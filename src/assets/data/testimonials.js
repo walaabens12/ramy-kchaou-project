676b369e5381377c9b4bc53c5fe6c7c1b5d26f71
salem <salem.dahmani345@gmail.com>
2021-12-20 22:08:41 +0100

    first commit

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Salem Dahmani',
+    title: 'CEO',
+    org: 'WEB CIFAR',
+    desc:
+      'Walaa is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+  },
+  {
+    id: 2,
+    name: 'Edem Gheriani',
+    title: 'Co-founder',
+    org: 'Kreetive',
+    desc:
+      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+  },
+  {
+    id: 3,
+    name: 'Bamri Selim',
+    title: 'Marketing Head',
+    org: 'Eleven Dawn',
+    desc:
+      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+  },
+];
+export default testimonials;
